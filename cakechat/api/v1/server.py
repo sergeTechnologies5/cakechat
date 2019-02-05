dc8e1da5e68bed229b16b93f96005b290051f477
njerunjru <njesh74@gmail.com>
2019-02-05 12:08:41 +0300

    1

---
+++ cakechat/api/v1/server.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+
+from cakechat.api.response import get_response
+from cakechat.api.utils import get_api_error_response, parse_dataset_param
+from cakechat.config import EMOTIONS_TYPES, DEFAULT_CONDITION
+from cakechat.utils.logger import get_logger
+from cakechat.utils.profile import timer
+
+_logger = get_logger(__name__)
+
+app = Flask(__name__)
+
+@app.route('/hello',methods=['GET'])
+def index():
+    return jsonify({'name':"ss"})
+
+@app.route('/cakechat_api/v1/actions/get_response', methods=['POST'])
+@timer
+def get_model_response():
+    params = request.get_json()
+    
+    _logger.info('request params: %s' % params)
+
+    try:
+        dialog_context = parse_dataset_param(params, param_name='context')
+    except KeyError as e:
+        return get_api_error_response('Malformed request, no "%s" param was found' % str(e), 400, _logger)
+    except ValueError as e:
+        return get_api_error_response('Malformed request: %s' % str(e), 400, _logger)
+
+    emotion = params.get('emotion', DEFAULT_CONDITION)
+
+    response = get_response(dialog_context, emotion)
+
+    if not response:
+        _logger.error('No response for context: %s; emotion "%s"' % (dialog_context, emotion))
+        return jsonify({}), 200
+
+    _logger.info('Given response: "%s" for context: %s; emotion "%s"' % (response, dialog_context, emotion))
+
+    return jsonify({'response': response}), 200
